0833a9cbc2406c463399548ca6d3f55d5b0c56cf
oeway <oeway007@gmail.com>
2019-04-25 10:44:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,25 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/precache-manifest.a13f67cb04e57ffe5810d60bbf8fcf97.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+if (workbox) {
+    console.log(`Workbox is loaded`);
+    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+    workbox.core.setCacheNameDetails({prefix: "ImJoy.io"});
 
-importScripts(
-  "/precache-manifest.a13f3523300866de7ef1548350bf157f.js"
-);
+    /**
+     * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+     * requests for URLs in the manifest.
+     * See https://goo.gl/S9QRab
+     */
+    self.__precacheManifest = [].concat(self.__precacheManifest || []);
+    workbox.precaching.suppressWarnings();
+    workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.core.setCacheNameDetails({prefix: "ImJoy.io"});
+    self.addEventListener("message", (e)=>{
+      if (e.data.action=='skipWaiting') self.skipWaiting()
+    })
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+}
+else {
+    console.log(`Workbox didn't load`);
+}
+
