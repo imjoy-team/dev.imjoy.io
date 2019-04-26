56b1a220c8e2535aa472a1a854099f8e3114e4e1
oeway <oeway007@gmail.com>
2019-04-26 12:03:50 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-importScripts("/precache-manifest.b2a5fead33bbe357613536700976070e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.11f47333d826e6dbdbff325db3beec09.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/* eslint-disable */
 
 if (workbox) {
     console.log(`Workbox is loaded`);
